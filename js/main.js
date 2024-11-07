338dd9327d33b39422ce8abfccc71f8f7fc48327
q1qe <quanganhp030@gmail.com>
2024-11-07 14:51:08 +0700

    Revert "improved about page and added style.css"
    This reverts commit 3e2bb22ec8e4f67834acda50bd2c4b32ec06fa51.

--- js/main.js
+++ js/main.js
@@ -1,102 +1,70 @@
 (function ($) {
     "use strict";
     
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
+    // Global animation settings
+    const animationSettings = {
+        scrollTiming: 800,
+        fadeInDelay: 200,
+        counterDuration: 2000,
+        hoverTransition: 300
+    };
+
+    // Initialize all animations
+    function initAnimations() {
+        $('.animate-on-scroll').each(function() {
+            const element = $(this);
+            const elementPosition = element.offset().top;
+            const windowHeight = $(window).height();
+            const scrollPosition = $(window).scrollTop();
+
+            if (elementPosition < (scrollPosition + windowHeight - 100)) {
+                element.addClass('animated');
+            }
+        });
+
+        // Initialize counters with synchronized timing
+        $('.counter').each(function(index) {
+            const $counter = $(this);
+            const delay = index * animationSettings.fadeInDelay;
+            
+            setTimeout(() => {
+                $counter.prop('Counter', 0).animate({
+                    Counter: $counter.text()
+                }, {
+                    duration: animationSettings.counterDuration,
+                    easing: 'swing',
+                    step: function(now) {
+                        $(this).text(Math.ceil(now));
+                    }
                 });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+            }, delay);
+        });
+    }
 
+    // Smooth scroll with synchronized timing
+    $('a[href*="#"]').on('click', function(e) {
+        if (this.hash !== '') {
+            e.preventDefault();
+            const hash = this.hash;
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-
-
-    // Post carousel
-    $(".post-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            }
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 70
+            }, animationSettings.scrollTiming, 'easeInOutExpo', function() {
+                window.location.hash = hash;
+            });
         }
     });
 
+    // Initialize on page load
+    $(document).ready(function() {
+        // Initial animation check
+        initAnimations();
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        center: true,
-        autoplay: true,
-        smartSpeed: 2000,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
+        // Check for animations on scroll
+        $(window).on('scroll', function() {
+            initAnimations();
+        });
     });
-    
+
 })(jQuery);
 
